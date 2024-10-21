03975b5ffe8bb85c5b3cdc782fd923b495fed4d8
phumentceo <roungchanrith8@gmail.com>
2024-10-21 10:33:00 +0700

    React with tutoreil

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,8 @@
 import React from 'react'
-import Intro01 from './components/introduction/Intro01'
-import Intro02 from './components/introduction/intro02'
-import Props01 from './components/props/Props01'
-import Condition01 from './components/condition/Condition01'
-import Condition02 from './components/condition/Condition02'
-import Condition03 from './components/condition/Condition03'
-import Condition04 from './components/condition/Condition04'
-
 const App = () => {
   return (
     <div>
-      {/* <Intro01/> */}
-
-      {/* <Intro02/>
-      <Props01 size={300}/>
-      <Props01 size={200}/>
-      <Props01 size={100}/> */}
-      <Condition01/>
-      <Condition02/>
-      <Condition03/>
-      <Condition04 status={true}/>
+      
     </div>
   )
 }

37c58094896a17b63d6b0a68329a715879dd9599
phumentceo <roungchanrith8@gmail.com>
2024-11-11 18:22:01 +0700

    React with state

--- src/components/hook/state/State05.jsx
+++ src/components/hook/state/State05.jsx
@@ -4,12 +4,15 @@ const State05 = () => {
 
   const [name,setName] = useState('');
   const [persons, setPersons] = useState([]);
+  const [edit,editMod] = useState(false);
+
+  const [personId,setPersonId] = useState(null);
 
   const handleInput = (e) => {
     setName(e.target.value);
   }
 
-  const handleClick = () => {
+  const handleSave = () => {
     if(name != ''){
         setPersons([...persons,name]);
         setName('');
@@ -17,25 +20,39 @@ const State05 = () => {
   }
 
   const handleDelete = (index) => {
-     alert(index);
+    if(confirm("Do you want to delete this?")){
+      setPersons(persons.filter((value,key) => key != index));
+    }
+  }
 
-     //index = 0
-     setPersons(persons.filter((value,key) => key != index));
 
-     /*
-       persons = ["lika","panha","reaksa"];
-     */
+  //Edit
+  const handleEdit = (index,item) => {
+    editMod(true)
+    setName(item)
+    setPersonId((value) => value + index)
+
+    console.log(personId);
+  }
+
+  const handleCancel = () => {
+    editMod(false)
+    setName('')
   }
 
   return (
     <div>
-         <form className=' w-50 mx-auto p-5 border bg-secondary rounded-3'>
+         <form className=' w-50 mx-auto p-5 border rounded-3'>
             <div className="form-group mb-3">
                 <label htmlFor="">Username</label>
-                <input type="text" name="" onInput={handleInput} className=" form-control shadow-none" placeholder='Enter person name' />
+                <input value={name} type="text" name="" onInput={handleInput} className=" form-control shadow-none" placeholder='Enter person name' />
             </div>
 
-            <button type='button' onClick={handleClick} className=' btn btn-success'>Add+</button>
+            <button type='button' onClick={handleSave} className=' btn btn-success'>
+              { edit == true ? 'Update' : 'Add+'}
+            </button>
+
+            <button onClick={handleCancel} className=' btn btn-danger ms-3' type='reset'>Cancel</button>
          </form>
 
          <div className=' w-50 mx-auto mb-5 mt-5'>
@@ -51,7 +68,12 @@ const State05 = () => {
                     <div key={index} className=' d-flex justify-content-between align-items-center mb-2 border-bottom pb-3'>
                         <h5>ID :   {index+1}</h5>
                         <h5>Name : {item}</h5>
-                        <button onClick={() => handleDelete(index)} className=' btn btn-danger btn-sm '>remove -</button>
+
+                        <div className="">
+                            <button onClick={() => handleEdit(index,item)} className=' btn btn-primary btn-sm mx-2'>Edit+</button>
+                            <button onClick={() => handleDelete(index)} className=' btn btn-danger btn-sm '>remove -</button>
+                        </div>
+                        
                     </div>
                 ))
             }

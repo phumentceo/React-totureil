c130b52abd3ad82005e4961a0f511ed940782a07
phumentceo <roungchanrith8@gmail.com>
2024-11-18 12:12:47 +0700

    State with localStorage

--- src/components/hook/state/State06.jsx
+++ src/components/hook/state/State06.jsx
@@ -1,17 +1,43 @@
-import React from 'react';  
+import React, { useState } from 'react';  
 
 const State06 = () => {  
+  const [persons,setPersons]  = useState(() => {
+    return JSON.parse(localStorage.getItem('persons')) || [];
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    let personData = new FormData(e.target);
+
+    // let name = personData.get('username');
+    // let gedner = personData.get('gender');
+    // let phone = personData.get('phone');
+
+    let person = {
+      'id' : Date.now(),
+      'username' : personData.get('username'),
+      'gender'   : personData.get('gender'),
+      'phone'    : personData.get('phone')
+    }
+    console.log(person);
+
+    setPersons([...persons,person]);
+    localStorage.setItem('persons',JSON.stringify(persons));
+    
+
+  }
   return (  
     <div className='w-50 container'>  
 
-      <form className='p-4 border'>  
+      <form onSubmit={handleSubmit} className='p-4 border'>  
         <div className="form-group mb-3">  
           <label>Username</label>  
-          <input type="text" name="name" className="form-control" required defaultValue={editingPerson ? editingPerson.name : ''}/>  
+          <input type="text" name="username" className="form-control"/>  
         </div>  
         <div className="form-group mb-3">  
           <label>Gender</label>  
-          <select name="gender" className="form-control" required defaultValue={editingPerson ? editingPerson.gender : ''}>  
+          <select name="gender" className="form-control">  
             <option value="">Select Gender</option>  
             <option value="male">Male</option>  
             <option value="female">Female</option>  
@@ -22,7 +48,8 @@ const State06 = () => {
           <label>Phone Number</label>  
           <input type="text" name="phone" className=' form-control'/>  
         </div>  
-        <button type="submit" className="btn btn-primary">{editingPerson ? 'Update' : 'Submit'}</button>  
+        <button type="submit" className="btn btn-primary me-2">Submit</button>  
+        <button type='reset' className=' btn btn-danger'>Reset</button>
       </form>  
 
       <div className='py-4'>  
